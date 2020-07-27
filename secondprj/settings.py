316fbd55d422bb47dc5f209f2aa23925d54fbc73
chanunga <chanunga@gmail.com>
2020-07-27 22:56:46 +0900

    commit

--- secondprj/settings.py
+++ secondprj/settings.py
@@ -20,12 +20,16 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'h$_kwyd&v^wnwi5$i@arp=zabr3h5&$@774y-sy+qh#8-dqi#o'
+
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'h$_kwyd&v^wnwi5$i@arp=zabr3h5&$@774y-sy+qh#8-dqi#o')
+# SECRET_KEY = 'h$_kwyd&v^wnwi5$i@arp=zabr3h5&$@774y-sy+qh#8-dqi#o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
-ALLOWED_HOSTS = []
+#ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
